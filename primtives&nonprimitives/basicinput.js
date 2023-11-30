5b5555cf5b9eefd4e56149de82d3fa3cd496ae37
Kanishak Goel <92166791+Knshq95@users.noreply.github.com>
2023-11-30 15:54:57 +0000

    input using prompt-sync and readline done

--- primtives&nonprimitives/basicinput.js
+++ primtives&nonprimitives/basicinput.js
@@ -8,5 +8,12 @@ console.log(a);
 
 /* 
 input using readline method
-
 */
+const readline = require('readline')
+const rl = readline.createInterface({
+    input:process.stdin,
+    output:process.stdout
+})
+rl.question('enter any value:',(input) => {
+    console.log(`your value is: ${input}`);
+})
