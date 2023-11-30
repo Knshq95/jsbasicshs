c9d9636357ff48948d26f1d5d564a190f6a00960
Kanishak Goel <92166791+Knshq95@users.noreply.github.com>
2023-11-30 15:36:43 +0000

    questions added and input figured out

---
+++ primtives&nonprimitives/basicinput.js
@@ -0,0 +1,12 @@
+// input using prompt-sync method
+//make sure node and npm are installed
+//run npm install prompt-sync
+
+const prompt = require('prompt-sync')()
+let a = prompt('enter any value:')
+console.log(a);
+
+/* 
+input using readline method
+
+*/
