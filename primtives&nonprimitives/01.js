0be5a714e466dade01f3bdaab9d85eed81132f83
Kanishak Goel <92166791+Knshq95@users.noreply.github.com>
2023-11-30 15:23:28 +0000

    inital setup and figuring how to take input in nodejs

---
+++ primtives&nonprimitives/01.js
@@ -0,0 +1,7 @@
+// input using prompt-sync method
+//make sure node and npm are installed
+//run npm install prompt-sync
+
+const prompt = require('prompt-sync')()
+let a = prompt('enter any value:')
+console.log(a);
